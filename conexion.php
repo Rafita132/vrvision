8a29104f1dbe0cdad5e77cc49d1a96d4d9f25860
Rafita132 <118165941+Rafita132@users.noreply.github.com>
2024-11-26 20:38:31 +0100

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,18 +1,15 @@
 <?php
-// Habilitar la visualización de errores
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Datos de conexión a la base de datos
 $host = "srv1582.hstgr.io";
 $username = "u554899566_usurafa";
 $password = "Bj9~yNBZVBz";
 $database = "u554899566_Dataservervr";
 
-// Crear la conexión
 $conn = new mysqli($host, $username, $password, $database);
 
-// Comprobar la conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
